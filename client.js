3ae53cfa517154beaf63c3ee017e518d8e6cd78c
rexhome7326 <rexhome7326@gmail.com>
2016-08-26 15:01:50 +0800

    replace console.error regex@0.3.3

--- client.js
+++ client.js
@@ -3,7 +3,7 @@
 var sourceConsoleError = console.error;
 console.error = function(){
 	if(typeof arguments[0] === 'string'){
-		if(/Unexpected keys|React attempted to reuse markup in a container/g.test(arguments[0])){
+		if(/Unexpected key(|s)|React attempted to reuse markup in a container/g.test(arguments[0])){
 			return;
 		}
 		
