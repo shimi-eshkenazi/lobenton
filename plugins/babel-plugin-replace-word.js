0358c9fe6eb01889149d8ff11737a78050db728a
rex.chen <rex.chen@104.com.tw>
2016-09-11 23:11:50 +0800

    test lib@0.13.1

--- plugins/babel-plugin-replace-word.js
+++ plugins/babel-plugin-replace-word.js
@@ -1,18 +1,40 @@
 "use strict";
 
 exports.default = function (babel) {
+  var checkFlag = process.argv.slice(-1).pop() === "--replace-word";
+
   return {
     visitor: {
-      ImportDeclaration(path) {
-        if(/^src\/.+/.test(path.node.source.value)){
-          path.node.source.value = path.node.source.value.replace(/^src\//g,"lib/");
+      ImportDeclaration(path, state) {
+        if(checkFlag === true){
+          var opts = Object.keys(state.opts);
+          
+          if(opts.length > 0){
+            opts.map(function loopOpts(source){
+              var replaceTo = state.opts[source];
+              var re1 = new RegExp(source, "gi");
+              
+              if(re1.test(path.node.source.value)){
+                path.node.source.value = path.node.source.value.replace(re1, replaceTo);
+              }
+            });
+          }
         }
       },
-      CallExpression(path) {
-        if(path.node.arguments.length > 0){
+      CallExpression(path, state) {
+        if(checkFlag === true && path.node.arguments.length > 0){
+          var opts = Object.keys(state.opts);
+          
           path.node.arguments.map(function loop(node, index){
-            if(node.type === "StringLiteral" && /^src\/.+/.test(node.value)){
-              node.value = node.value.replace(/^src\//g,"lib/");
+            if(opts.length > 0){
+              opts.map(function loopOpts(source){
+                var replaceTo = state.opts[source];
+                var re1 = new RegExp(source, "gi");
+                
+                if(node.type === "StringLiteral" && re1.test(node.value)){
+                  node.value = node.value.replace(re1, replaceTo);
+                }
+              });
             }
           });
         }
