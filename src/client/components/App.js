d97a508a36d4eae2488b48ca888e8c66dd1b47c3
rex.chen <rex.chen@104.com.tw>
2016-11-07 19:07:50 +0800

    fix bug for getParamMap

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -5,15 +5,10 @@ import React, { Component } from 'react';
 class App extends Component {
 	constructor( props, context ){
 		super( props, context );
-		this.paramMap = {};
 	}
 	
 	getParamMap(){
-		if(!this.paramMap || (Object.keys(this.paramMap).length === 0 && typeof this.props.params !== 'undefined' && typeof this.props.location !== 'undefined')){
-			this.paramMap = Object.assign({}, this.props.params, this.props.location.query);
-		}
-		
-		return this.paramMap;
+		return Object.assign({}, this.props.params, this.props.location.query);
 	}
 	
 	render() {
